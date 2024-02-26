faa2b795b52b231318cfb27ef1cb4b615ed4a5f5
Mateus Junior <mateus@matj.dev>
2024-02-26 14:47:14 +0100

    Update install.sh script to use current user and home directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,15 +2,10 @@
 
 set -e
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: ./install.sh <user> <home>"
-    exit 1
-fi
-
 echo "Installing base packages"
 
 sudo ./install-packages.sh
 
 echo "Installing essential packages"
 
-sudo USER="$1" HOME="$2" ./install-essential.sh
+sudo USER="$(whoami)" HOME="$HOME" ./install-essential.sh
