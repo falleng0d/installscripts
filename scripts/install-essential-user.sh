1993b8cea11cfb23f7549ab7405de737c3be181d
Mateus Junior <mateus@matj.dev>
2025-06-05 12:16:10 +0200

    Remove pip and setuptools upgrade commands from install-essential-user.sh

--- scripts/install-essential-user.sh
+++ scripts/install-essential-user.sh
@@ -31,8 +31,5 @@ brew install python@3.12
 ln -sf /home/linuxbrew/.linuxbrew/bin/python3.12 /home/linuxbrew/.linuxbrew/bin/python3
 ln -sf /home/linuxbrew/.linuxbrew/bin/pip3.12 /home/linuxbrew/.linuxbrew/bin/pip3
 
-python3 -m pip install --upgrade pip --break-system-packages
-python3 -m pip install --upgrade setuptools --break-system-packages
-
 # install python packages
 brew install ansible poetry fzf
